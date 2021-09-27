646dfc3a6915d444cc8aec6067c682116fc007cf
BaeSeokJin <htdocs16bsj@naver.com>
2021-09-27 21:45:17 +0900

    [배석진] 관리자페이지 컨트롤러 수정2

--- src/khs/adminPage/controller/adminPageController.java
+++ src/khs/adminPage/controller/adminPageController.java
@@ -19,7 +19,8 @@ public class adminPageController extends HttpServlet {
 		String[] uriArr = request.getRequestURI().split("/");
 		switch (uriArr[uriArr.length-1]) {
 		//1. 메인페이지에서 회원정보 조회 페이지로 들어갈 경우
-		case "accountInfo":
+		case "adminPage": 
+			adminPage(request,response);
 			break;
 		//2. 메인페이지에서 차단된 회원관리 페이지로 들어갈 경우
 		case "memberBlock":
@@ -30,6 +31,10 @@ public class adminPageController extends HttpServlet {
 		default:
 			break;
 		}
+	}
+
+	private void adminPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/adminPage/adminPage-index").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
