062f23e4df30a5625f3cc7802a4dcc28dd6bc093
BaeSeokJin <htdocs16bsj@naver.com>
2021-09-30 23:33:28 +0900

    [배석진] 관리자게시판 학습게시판 다시 추가

---
+++ src/khs/stduyPage/model/dao/StudyPageDao.java
@@ -0,0 +1,11 @@
+package khs.stduyPage.model.dao;
+
+import khs.common.db.JDBCTemplate;
+
+public class StudyPageDao {
+
+	private JDBCTemplate template = JDBCTemplate.getInstance();
+	
+	// 메서드 1. 데이터 그냥 불러오기
+	
+}
