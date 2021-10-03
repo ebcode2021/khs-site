40847b5b0529efe821554786ad02cc21922f23ea
son <ebcode2021@gmail.com>
2021-10-03 16:36:29 +0900

    [손은비] commit

--- src/khs/common/code/BanGrade.java
+++ src/khs/common/code/BanGrade.java
@@ -4,7 +4,7 @@ public enum BanGrade {
 	B01("차단해제", 0), //차단이 되어있지 않은 상태(default)
 	B02("1일차단", 1),
 	B03("7일차단", 7),
-	B04("영구차단", 10000);
+	B04("영구차단", 10000); 
 	
 	public final String banGrade; // 차단등급
 	public final int BanDate; // 차단날짜
